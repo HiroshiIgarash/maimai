f2a600c0452be91df1ef58a3f256f833f6d68dbc
HiroshiIgarash <tubukiti2008@gmail.com>
2025-08-24 09:59:30 +0900

    title-map.jsonの移動

--- app/utils/music-search.ts
+++ app/utils/music-search.ts
@@ -1,46 +1,6 @@
 import { MusicDifficulty, MusicRatingInfo, PlayInfo } from "./types/type";
 import { calculateSingleRating } from "./rating";
-
-const titleMap: { [key: string]: string } = {
-  "バラバラ〜仮初レインボーローズ〜": "バラバラ",
-  "アノーイング！さんさんウィーク！": "アノーイング！",
-  ようこそジャパリパークへ: "ジャパリパーク",
-  "にっこり^^調査隊のテーマ": "にっこり調査隊",
-  "みくみくにしてあげる♪【してやんよ】": "みくみくにしてあげる",
-  Link: "Link (nico)",
-  "Bad Apple!! feat nomico": "Bad Apple!!",
-  星界ちゃんと可不ちゃんのおつかい合騒曲: "おつかい合騒曲",
-  "ギリギリ最強あいまいみー！": "あいまいみー",
-  "偉大なる悪魔は実は大天使パトラちゃん様なのだ！":
-    "偉大なる悪魔は大天使パトラ",
-  "チュルリラ・チュルリラ・ダッダッダ！": "チュルリラ",
-  "メイトなやつら（FEAT. 天開司, 佐藤ホームズ, あっくん大魔王 & 歌衣メイカ）":
-    "メイトなやつら",
-  "FREEDOM DiVE (tpz Overcute Remix)": "FREEDOME Dive(tpz)",
-  エンドマークに希望と涙を添えて: "エンドマーク",
-  "若い力 -SEGA HARD GIRLS MIX-": "若い力(セハガール)",
-  "セハガガガンバッちゃう！！": "セハガール",
-  "ガチャガチャきゅ～と・ふぃぎゅ@メイト": "ガチャガチャきゅ～と",
-  "トリドリ⇒モリモリ！Lovely fruits☆": "Lovely fruits",
-  単一指向性オーバーブルーム: "オーバーブルーム",
-  "撩乱乙女†無双劇": "撩乱乙女無双劇",
-  魔理沙は大変なものを盗んでいきました: "魔理沙",
-  "キャプテン・ムラサのケツアンカー": "ケツアンカー",
-  "Bad Apple!! feat.nomico ～五十嵐 撫子 Ver.～": "Bad Apple(撫子)",
-  "Help me, ERINNNNNN!!": "Help me, ERIN!!(new)",
-  "ウッーウッーウマウマ(ﾟ∀ﾟ)": "ウマウマ",
-  "Bad Apple!! feat.nomico (Tetsuya Komuro Remix)": "Bad Apple!! (TK)",
-  "Help me, ERINNNNNN!!（Band ver.）": "Help me, ERIN!!（Band）",
-  スカーレット警察のゲットーパトロール24時: "スカーレット警察",
-  "チルノのパーフェクトさんすう教室　⑨周年バージョン": "チルノ9周年",
-  "Bad Apple!! feat.nomico": "Bad Apple!!",
-  "東方スイーツ！～鬼畜姉妹と受難メイド～": "東方スイーツ",
-  "ピポピポ -People People- feat. ななひら": "ピポピポ",
-  アトロポスと最果の探究者: "アトロポス",
-  "true my heart -Lovable mix-": "true my heart",
-  Löschen: "Loschen",
-  "U&iVERSE -銀河鸞翔-": "UNiVERSE 銀河鸞翔",
-};
+import titleMap from "../data/title-map.json";
 
 /**
  * 新しい楽曲データから譜面定数および旧曲・新曲を検索する
@@ -63,7 +23,7 @@ export const searchRealLevelAndIsNewFromMusicData = (
   level: number | undefined;
   isNew: boolean | undefined;
 } => {
-  const title = titleMap[musicTitle] || musicTitle;
+  const title = (titleMap as Record<string, string>)[musicTitle] || musicTitle;
 
   const difficultyData = musicData[difficulty];
   if (!difficultyData) {
